51ba251193c5dc4ff24d4bc2ffa119e9a57648a1
Mikiya <a543525063@qq.com>
2021-11-23 14:29:49 +0800

    leetcode 495

---
+++ LeetCode/495.go
@@ -0,0 +1,14 @@
+func findPoisonedDuration(timeSeries []int, duration int) int {
+	poisonedDuration := 0
+
+	for i := 0; i < len(timeSeries)-1; i++ {
+		if interval := timeSeries[i+1] - timeSeries[i]; interval >= duration {
+			poisonedDuration += duration
+		} else {
+			poisonedDuration += interval
+		}
+	}
+	poisonedDuration += duration
+
+	return poisonedDuration
+}
