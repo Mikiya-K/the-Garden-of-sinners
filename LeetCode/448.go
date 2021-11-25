0654e372c138aaf79580b273d317c1e63ae652f9
Mikiya <a543525063@qq.com>
2021-11-25 10:40:17 +0800

    update 448

--- LeetCode/448.go
+++ LeetCode/448.go
@@ -1,22 +1,18 @@
 package leetcode
 
 func findDisappearedNumbers(nums []int) []int {
-	for k, v := range nums {
-		nums[k] = -v
-	}
-
 	for _, v := range nums {
 		switch {
-		case v < 0 && nums[-v-1] < 0:
-			nums[-v-1] = -nums[-v-1]
-		case v > 0 && nums[v-1] < 0:
+		case v > 0 && nums[v-1] > 0:
 			nums[v-1] = -nums[v-1]
+		case v < 0 && nums[-v-1] > 0:
+			nums[-v-1] = -nums[-v-1]
 		}
 	}
 
 	result := make([]int, 0)
 	for k, v := range nums {
-		if v < 0 {
+		if v > 0 {
 			result = append(result, k+1)
 		}
 	}
