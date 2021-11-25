772bd804ecc7de611ff45a4c9af03069d835836d
Mikiya <a543525063@qq.com>
2021-11-25 10:04:10 +0800

    leetcode 448

---
+++ LeetCode/448.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func findDisappearedNumbers(nums []int) []int {
+	for k, v := range nums {
+		nums[k] = -v
+	}
+
+	for _, v := range nums {
+		switch {
+		case v < 0 && nums[-v-1] < 0:
+			nums[-v-1] = -nums[-v-1]
+		case v > 0 && nums[v-1] < 0:
+			nums[v-1] = -nums[v-1]
+		}
+	}
+
+	result := make([]int, 0)
+	for k, v := range nums {
+		if v < 0 {
+			result = append(result, k+1)
+		}
+	}
+
+	return result
+}
